4560c1ba7984186cc1746dc410242638af6984f7
scorjeff <jf.boisgontier@gmail.com>
2025-04-14 08:40:10 +0200

    Creation next config js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      allowedDevOrigins: ['http://192.168.1.246:3000'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
