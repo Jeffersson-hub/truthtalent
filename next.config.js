943618fd9460e0090f26e002f5cca8427705fb74
scorjeff <jf.boisgontier@gmail.com>
2025-04-17 08:14:52 +0200

    modif next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig}
 const nextConfig = {
           allowedDevOrigins: ['http://192.168.1.246:3000'],
   };
   
   module.exports = nextConfig;
-  
+*/
+// next.config.js
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
