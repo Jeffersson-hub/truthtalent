76d719cc937c9fac5d30339eff3a6de8bf976643
scorjeff <jf.boisgontier@gmail.com>
2025-04-17 08:08:20 +0200

    suppr expérimental next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      allowedDevOrigins: ['http://192.168.1.246:3000'],
-    },
+          allowedDevOrigins: ['http://192.168.1.246:3000'],
   };
   
   module.exports = nextConfig;
