19d8726c9bfa9d27994486363e79933b75119080
scorjeff <jf.boisgontier@gmail.com>
2025-04-18 07:39:11 +0200

    maj next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
-/** @type {import('next').NextConfig}
-const nextConfig = {
-          allowedDevOrigins: ['http://192.168.1.246:3000'],
-  };
-  
-  module.exports = nextConfig;
-*/
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ];
+  },
+}
+/**
 // next.config.js
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
+*/
