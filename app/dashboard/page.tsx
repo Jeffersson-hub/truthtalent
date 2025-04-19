bf1f86fd8ca198ada2e8c7d1575bd64e6b010b17
scorjeff <jf.boisgontier@gmail.com>
2025-04-19 09:33:34 +0200

    text page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,4 @@
-/* export default function Dashboard() {
-    return (
-      <div>
-        Dashboard Page
-      </div>
-    );
-  }
-   */
-  import React from 'react';
-
-const DashboardPage = () => {
-  return (
-    <div>
-      <h1>Tableau de bord</h1>
-      {/* Votre contenu ici */}
-    </div>
-  );
-};
-
-export default DashboardPage;
+// app/page.tsx
+export default function Home() {
+  return <h1>Bienvenue sur TruthTalent</h1>;
+}
